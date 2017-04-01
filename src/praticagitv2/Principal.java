d62e0e8e75b49128482d6c18fe4e6578fdb1043d
ALUNO <ALUNO@C-LAB05-PC03>
2017-03-31 21:36:38 -0300

    vers3

--- src/praticagitv2/Principal.java
+++ src/praticagitv2/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git...");
-        // crinado release com tag
+        // criando release com tag
     }
     
 }
